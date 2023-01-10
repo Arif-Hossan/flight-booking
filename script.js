f0384c53e96e6ed8adeb897b813ea27249e5b580
Arif Hossan <arif.hossan.eu@gmail.com>
2023-01-11 01:06:39 +0600

    ticket increase-decrease event added using js

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
-let a= 5;
-console.log(a);
+let firstClassPrice = 150;
+let economyClassPrice = 100;
+
+function handleTicketChange(ticketClass, isChange) {
+  let firstClassCount = document.getElementById(ticketClass + "-count");
+  let firstClassNumber = parseInt(firstClassCount.value);
+  let firstClassNewCount = firstClassNumber;
+  if (isChange == true) {
+    firstClassNewCount = firstClassNumber + 1;
+  }
+  if (isChange == false && firstClassNumber > 0) {
+    firstClassNewCount = firstClassNumber - 1;
+  }
+  document.getElementById(ticketClass + "-count").value = firstClassNewCount;
+}
+
+// document
+//   .getElementById("first-class-increase")
+//   .addEventListener("click", function () {
+//     let firstClassCount = document.getElementById("first-class-count");
+//     let firstClassNumber = parseInt(firstClassCount.value);
+//     let firstClassNewCount = firstClassNumber + 1;
+//     document.getElementById("first-class-count").value = firstClassNewCount;
+//   });
+
+// document
+//   .getElementById("first-class-decrease")
+//   .addEventListener("click", function () {
+//     let firstClassCount = document.getElementById("first-class-count");
+//     let firstClassNumber = parseInt(firstClassCount.value);
+//     let firstClassNewCount = firstClassNumber - 1;
+//     document.getElementById("first-class-count").value = firstClassNewCount;
+//   });
